d37b39801e0ef5ceb651ea14bbba3178531c43d4
xkasiaaa3 <104648382+xkasiaaa3@users.noreply.github.com>
2023-03-30 18:09:59 +0200

    zad5

--- index.js
+++ index.js
@@ -9,12 +9,13 @@
 
 const express = require("express");
 const questions = require("./questions").questions;
+const config = require("./config").config;
 const app = express();
 
 app.get("/api/questions", (request, response) => {
   response.send(questions);
 });
 
-app.listen(3000, function () {
+app.listen(config.port, function () {
   console.info("Server is running at port 3000");
 });
