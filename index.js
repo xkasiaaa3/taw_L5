99a90fec103636fba566109c4517dd1fdfb9e6a1
xkasiaaa3 <104648382+xkasiaaa3@users.noreply.github.com>
2023-03-30 18:06:25 +0200

    zad4

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const http = require("http");
+// const http = require('http');
 
-const server = http.createServer();
-server.on("connection", (socket) => {
-  console.log("new connection");
+// const server = http.createServer();
+// server.on('connection', (socket => {
+//     console.log('new connection');
+// }))
+
+// server.listen(3000);
+
+const express = require("express");
+const questions = require("./questions").questions;
+const app = express();
+
+app.get("/api/questions", (request, response) => {
+  response.send(questions);
 });
 
-server.listen(3000);
+app.listen(3000, function () {
+  console.info("Server is running at port 3000");
+});
