514b620e1ecae7abea5c2b26a48d1983bf43a6a6
xkasiaaa3 <104648382+xkasiaaa3@users.noreply.github.com>
2023-05-04 10:49:23 +0200

    zad2

---
+++ rectangle_zad2.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Point = void 0;
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    move(dx, dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+}
+exports.Point = Point;
+class Rectangle {
+    constructor(topLeft, topRight, bottomLeft, bottomRight) {
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+    move(dx, dy) {
+        this.topLeft.move(dx, dy);
+        this.topRight.move(dx, dy);
+        this.bottomLeft.move(dx, dy);
+        this.bottomRight.move(dx, dy);
+    }
+    getArea() {
+        let width = this.topRight.x - this.topLeft.x;
+        let height = this.topRight.y - this.bottomRight.y;
+        return width * height;
+    }
+}
