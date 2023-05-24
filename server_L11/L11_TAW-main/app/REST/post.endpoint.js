4fcbeaa436a9b6d9d6f3513e6b5f7bc295f94e24
xkasiaaa3 <104648382+xkasiaaa3@users.noreply.github.com>
2023-05-24 11:04:47 +0200

    zad2

---
+++ server_L11/L11_TAW-main/app/REST/post.endpoint.js
@@ -0,0 +1,12 @@
+import business from "../business/business.container";
+const postEndpoint = (router) => {
+  router.get("/api/posts", async (request, response, next) => {
+    try {
+      let result = await business.getPostManager().query();
+      response.status(200).send(result);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+export default postEndpoint;
--- server_L11/L11_TAW-main/app/REST/routes.js
+++ server_L11/L11_TAW-main/app/REST/routes.js
@@ -1,7 +1,9 @@
-import userEndpoint from './user.endpoint';
+import userEndpoint from "./user.endpoint";
+import postEndpoint from "./post.endpoint";
 
 const routes = function (router) {
-    userEndpoint(router);
+  userEndpoint(router);
+  postEndpoint(router);
 };
 
 export default routes;
